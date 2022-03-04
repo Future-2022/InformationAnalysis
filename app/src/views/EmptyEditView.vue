25fcaf14ba7c49dd8e6316409208c5ef4294ab83
Roman Fedco <future55star@mail.ru>
2022-03-04 10:37:08 +0300

    fix title

--- app/src/views/EmptyEditView.vue
+++ app/src/views/EmptyEditView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class='text-center pt-5'>
-    <h1 class='grey'> The data that should be edited hasn't selected!</h1>
+    <h1 class='grey'> The data that should be edited hasn't been selected!</h1>
   </div>
 </template>
 
